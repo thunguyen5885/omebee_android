c44e5971cbfb015e05c6003f2b9df42c4cacdf6b
thunguyen5885 <thunguyen5885@gmail.com>
2014-08-09 15:01:38 +0700

    Fix navigation drawer

--- Omebee/app/src/main/java/com/omebee/android/layers/ui/ProductsLauncherActivity.java
+++ Omebee/app/src/main/java/com/omebee/android/layers/ui/ProductsLauncherActivity.java
@@ -1,8 +1,12 @@
 package com.omebee.android.layers.ui;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.omebee.android.R;
@@ -15,9 +19,13 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import java.util.Locale;
 
 /**
  * Created by phan on 8/6/2014.
@@ -28,30 +36,33 @@ public class ProductsLauncherActivity extends BaseActivity implements IProductsL
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
+
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
-    private String[] mMenuTitles;
+    private String[] mPlanetTitles;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTitle = mDrawerTitle = getTitle();
-
-        mMenuTitles = getResources().getStringArray(R.array.product_menu_array);
         mProductsLauncherPresenter = new ProductsLauncherPresenterImpl(this);
         mProductsLauncherFragment = (ProductsLauncherFragment)getFragmentManager().findFragmentById(R.id.productsLauncherFragment);
         if(mProductsLauncherFragment != null) {
             mProductsLauncherFragment.setPresenter(mProductsLauncherPresenter);
         }
+        mTitle = mDrawerTitle = getTitle();
+        mPlanetTitles = getResources().getStringArray(R.array.product_menu_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        // set up the drawer's list view with items and click listener
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mMenuTitles));
-        mDrawerList.setOnItemClickListener(this);
+
         // set a custom shadow that overlays the main content when the drawer opens
         mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        // Always hidden title
+        getActionBar().setTitle("");
         // enable ActionBar app icon to behave as action to toggle nav drawer
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
@@ -64,36 +75,63 @@ public class ProductsLauncherActivity extends BaseActivity implements IProductsL
                 R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
                 R.string.drawer_open,  /* "open drawer" description for accessibility */
                 R.string.drawer_close  /* "close drawer" description for accessibility */
-        ) {
-            public void onDrawerClosed(View view) {
-                getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
+        );
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         if (savedInstanceState == null) {
             selectItem(0);
         }
     }
-    private void selectItem(int position) {
-        // update the main content by replacing fragments
-//        mProductsLauncherFragment.selectItem(position);
 
-        // update selected item and title, then close the drawer
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.abmenu_products_launcher, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action itsible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action buttons
+        switch(item.getItemId()) {
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+//
+//        // update selected item and title, then close the drawer
+        mProductsLauncherFragment.selectItem(position);
         mDrawerList.setItemChecked(position, true);
-        setTitle(mMenuTitles[position]);
+        setTitle(mPlanetTitles[position]);
         mDrawerLayout.closeDrawer(mDrawerList);
     }
+
     @Override
     public void setTitle(CharSequence title) {
         mTitle = title;
-        getActionBar().setTitle(mTitle);
+//        getActionBar().setTitle(mTitle);
     }
 
     /**
@@ -114,34 +152,7 @@ public class ProductsLauncherActivity extends BaseActivity implements IProductsL
         // Pass any configuration change to the drawer toggls
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
-    @Override
-    public boolean onCreateOptionsMenu( Menu menu )
-    {
-        getMenuInflater().inflate( R.menu.abmenu_products_launcher, menu );
-        return true;
-    }
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        return super.onPrepareOptionsMenu(menu);
-    }
-    @Override
-    public boolean onOptionsItemSelected( MenuItem item )
-    {
-        boolean ret;
-        if (item.getItemId() == R.id.menu_account)
-        {
-            // Handle Settings
 
-            ret = true;
-        }
-        else
-        {
-            ret = super.onOptionsItemSelected( item );
-        }
-        return ret;
-    }
 
     public IProductsLauncherPresenter getProductsLauncherPresenter() {
         return mProductsLauncherPresenter;
